5a1f2778c87ddb87d746a494c34dce6bd71b82e5
Ebrahim112002 <md1847094@gmail.com>
2025-06-23 22:54:02 +0600

    added sign and login interface

--- Agriculture_management_shop/src/agriculture_management_shop/FXMLDocumentController.java
+++ Agriculture_management_shop/src/agriculture_management_shop/FXMLDocumentController.java
@@ -6,10 +6,18 @@ package agriculture_management_shop;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 /**
  *
@@ -17,14 +25,63 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
+    @FXML
+    private AnchorPane loginForm;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField pass;
+    @FXML
+    private Hyperlink forgot_pass;
+    @FXML
+    private Button loginBtn;
+    @FXML
+    private AnchorPane signupForm;
+    @FXML
+    private TextField Ruser;
+    @FXML
+    private PasswordField Rpass;
+    @FXML
+    private ComboBox<?> Rquestion;
+    @FXML
+    private TextField Ranswer;
+    @FXML
+    private Button signupBtn;
+    @FXML
+    private AnchorPane side_form;
+    @FXML
+    private Button side_createBtn;
+    @FXML
+    private Button side_alreadyHave;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+   public void switchForm(ActionEvent event){
+      TranslateTransition slider = new TranslateTransition();
+      
+       if(event.getSource()==side_createBtn){
+           slider.setNode(side_form);
+           slider.setToX(350);
+           slider.setDuration(Duration.seconds(.5));
+           
+           slider.setOnFinished((ActionEvent e)->{
+           side_alreadyHave.setVisible(true);
+           side_createBtn.setVisible(false);
+       });
+           slider.play();
+       }else if(event.getSource()==side_alreadyHave){
+             slider.setNode(side_form);
+           slider.setToX(0);
+           slider.setDuration(Duration.seconds(.5));
+           
+           slider.setOnFinished((ActionEvent e)->{
+           side_alreadyHave.setVisible(false);
+           side_createBtn.setVisible(true);
+       });
+           slider.play();
+       }
+   
+   }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
